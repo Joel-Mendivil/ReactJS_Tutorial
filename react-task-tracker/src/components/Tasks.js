cb74963da19303158b0baf3552c4dd111b274817
Joel Mendivil <jmendivil171@west-mec.org>
2021-05-18 15:21:26 -0700

    ReactJS Tutorial (Video) Day 2

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,11 +1,11 @@
 import Task from './Tasks'
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map(() => (
                 <Task key={task.id} task={task}
-                onDelete={onDelete}/>
+                onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )
